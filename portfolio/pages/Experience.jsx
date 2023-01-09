77e6dca9eee852ba3f1249dc65daf7d34311c8c6
PKmasai <prakashorious@gmail.com>
2023-01-09 13:06:28 +0530

    portfolio

---
+++ portfolio/pages/Experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function experience() {
+    return (
+        <div style={{
+            width: '100%', backgroundColor: 'grey'
+        }}>
+            <h3>Trainee Full StackDeveloper-Masai School</h3>
+            <p>jan 2022 - Dec-2022</p>
+            
+        </div>
+    )
+}
